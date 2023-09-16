1cdeb5cc6a23e34a64b7f10e50faf85ac6375655
riki-k <56576188+riki-k@users.noreply.github.com>
2023-09-16 17:48:06 +0200

    change asteroids behave

--- Assets/MovingAsteroids.cs
+++ Assets/MovingAsteroids.cs
@@ -8,6 +8,10 @@ public class MovingAsteroids : MonoBehaviour
     public int asteroids_counter;
     private float half_x_size_camera;
     private float half_y_size_camera;
+    private float x_left_borders;
+    private float y_bottom_borders;
+
+    private SpriteRenderer playersprite;
 
     Vector3 borders;
 
@@ -17,6 +21,9 @@ public class MovingAsteroids : MonoBehaviour
         borders = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
         half_x_size_camera = (Main.main.gameCamera.transform.position.x - borders.x);
         half_y_size_camera = (Main.main.gameCamera.transform.position.y - borders.y);
+        x_left_borders = Main.main.gameCamera.transform.position.x + half_x_size_camera;
+        y_bottom_borders = Main.main.gameCamera.transform.position.y + half_y_size_camera;
+        playersprite = GetComponent<SpriteRenderer>();
         speed = Random.Range(5, 10);
     }
 
@@ -24,15 +31,31 @@ public class MovingAsteroids : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.right * Time.deltaTime * speed);
+        toroidal_space();
+    }
 
-        if ((transform.position.x + 10) < (Main.main.gameCamera.transform.position.x + half_x_size_camera) || 
-            (transform.position.x - 10) > borders.x || 
-            (transform.position.y - 10) > borders.y || 
-            (transform.position.y + 10) < (Main.main.gameCamera.transform.position.y + half_y_size_camera))
+    void toroidal_space()
+    {
+        if (transform.position.x + (playersprite.size.x / 2) < x_left_borders)
         {
-            Destroy(gameObject);
-            FindObjectOfType<SpawnManager>().asteroids_counter--;
+            transform.position = new Vector3(borders.x + (playersprite.size.x / 2), transform.position.y, transform.position.z);
         }
+
+        if (transform.position.x - (playersprite.size.x / 2) > borders.x)
+        {
+            transform.position = new Vector3(x_left_borders - (playersprite.size.x / 2), transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.y - (playersprite.size.x / 2) > borders.y)
+        {
+            transform.position = new Vector3(transform.position.x, y_bottom_borders - (playersprite.size.x / 2), transform.position.z);
+        }
+
+        if (transform.position.y + (playersprite.size.x / 2) < y_bottom_borders)
+        {
+            transform.position = new Vector3(transform.position.x, borders.y + (playersprite.size.x / 2), transform.position.z);
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
