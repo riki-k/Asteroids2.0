8014b3e5345429f6091b9ddc299a43bbbbc6f604
riki-k <56576188+riki-k@users.noreply.github.com>
2023-10-20 16:02:12 +0200

    Update GameOver behaviour

--- Assets/Script/GameOver.cs
+++ Assets/Script/GameOver.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameOver : MonoBehaviour
 {
+    public TMP_Text Title;
+    public GameObject InputField;
+    public GameObject newGameButton;
+    public GameObject highScoreButton;
+    public GameObject exitButton;
+    public GameObject saveButton;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        newGameButton.SetActive(false);
+        highScoreButton.SetActive(false);
+        exitButton.SetActive(false);
 
+        saveButton.SetActive(true);
+        Title.enabled = true;
+        InputField.SetActive(true);
     }
 
     // Update is called once per frame
@@ -15,6 +30,7 @@ public class GameOver : MonoBehaviour
     {
 
     }
+
     public void LoadNewgame()
     {
         FSM.fsm.state = FSM.gamestate.play;
@@ -22,5 +38,17 @@ public class GameOver : MonoBehaviour
     public void HighScoreScene()
     {
         FSM.fsm.state = FSM.gamestate.highScore;
+    }
+
+    public void Savedata()
+    {
+        Main.main.playerName = InputField.GetComponent<TMP_InputField>().text;
+        saveButton.SetActive(false);
+        Title.enabled = false;
+        InputField.SetActive(false);
+
+        newGameButton.SetActive(true);
+        highScoreButton.SetActive(true);
+        exitButton.SetActive(true);
     }
 }
